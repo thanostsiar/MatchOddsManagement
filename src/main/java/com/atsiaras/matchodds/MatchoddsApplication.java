3c53fa31a2f4fb6f2e1e930fed5756e73170931d
Thanasis Tsiaras <thanostsiar@gmail.com>
2025-09-26 00:27:11 +0300

    Initial commit

---
+++ src/main/java/com/atsiaras/matchodds/MatchoddsApplication.java
@@ -0,0 +1,13 @@
+package com.atsiaras.matchodds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchoddsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchoddsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsiaras/matchodds/MatchoddsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsiaras.matchodds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchoddsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
